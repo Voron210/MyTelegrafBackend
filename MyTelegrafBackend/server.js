6330f8879c0f092dede3f50ddeeff3b663344e93
Stanislav Savinetskii <Savinesav@gmail.com>
2023-02-19 17:07:36 +0300

    /api

--- MyTelegrafBackend/server.js
+++ MyTelegrafBackend/server.js
@@ -4,12 +4,15 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const PORT = process.env.PORT //1488
 const cors = require('cors')
+const helmet = require('helmet');
+
 const router = require('./routes/index')
 
 const errorHandler = require('./middleware/errorHandlingMiddleware')
 
 const app = express()
-app.use(cors())
+app.use(helmet());
+//app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
